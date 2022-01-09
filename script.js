069367b55bfefcd3a26bc62108dfda28d2d3f3f1
PMSP31 <pmsp3108@gmail.com>
2022-01-09 22:09:02 +0700

    add new features, repair responsive for mobile

--- script.js
+++ script.js
@@ -2,6 +2,25 @@
 const btn = document.querySelector("button.mobile-menu-button");
 const menu = document.querySelector(".mobile-menu");
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", function () {
   menu.classList.toggle("hidden");
 });
+
+function submitForm(e) {
+  swal({
+    title: "Success!",
+    text: "Your message has been sent!",
+    icon: "success",
+  });
+  document.getElementById("form-contact").reset();
+  e.preventDefault();
+}
+
+function order(e) {
+  swal({
+    title: "Hey!",
+    text: "Currently, this feature cannot be used.",
+    icon: "info",
+  });
+  e.preventDefault();
+}
